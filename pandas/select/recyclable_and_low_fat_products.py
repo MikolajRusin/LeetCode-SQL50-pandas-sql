352e4f825bea23ee05d6849bb3bdfa31deeff488
MikolajRusin <rusinmiko@gmail.com>
2025-11-28 15:30:51 +0100

    add first solutions for select

--- pandas/select/recyclable_and_low_fat_products.py
+++ pandas/select/recyclable_and_low_fat_products.py
@@ -5,3 +5,12 @@ def find_products(products: pd.DataFrame) -> pd.DataFrame:
 def find_products(products: pd.DataFrame) -> pd.DataFrame:
     keep_mask = (products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')
     return products.loc[keep_mask, ['product_id']]
+
+#Or pipeline way
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    filtered_products = (
+        products
+        .query("low_fats == 'Y' & recyclable == 'Y'")
+        .loc[:, ['product_id']]
+    )
+    return filtered_products
